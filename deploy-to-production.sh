e8c773106a699de4a56dbc346367df16e756f471
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-08-04 00:35:00 +0700

    Initial deploy script

---
+++ deploy-to-production.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Simple Hello World bash script
+echo "Hello, World!"
+
+git fetch
+git checkout main
+git pull origin main
+git checkout prod
+git pull origin prod
+
+git merge --ff-only main
+# if [ $? -eq 0 ]; then
+#     echo "Merge successful. Pushing changes to prod branch."
+#     git push origin prod
+#     git checkout main
+#     git branch -d prod
+# else
+#     echo "Merge failed. Please resolve conflicts and try again."
+# fi
